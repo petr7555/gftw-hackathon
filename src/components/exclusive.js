4c2300cdbfb2c3ab724e350ceda36f5006cee597
Petr Janik <485122@mail.muni.cz>
2020-05-25 23:06:10 +0200

    Add document undefined check

--- src/components/exclusive.js
+++ src/components/exclusive.js
@@ -7,17 +7,19 @@ export const Exclusive = ({ children }) => {
   const [visible, setVisible] = useState(false)
   const [alwaysVisible, setAlwaysVisible] = useState(false)
 
-  if (document.monetization) {
-    document.monetization.addEventListener("monetizationstart", () => {
-      console.log("Enabled web monetization")
-      setVisible(true)
-    })
+  if (typeof document !== "undefined") {
+    if (document.monetization) {
+      document.monetization.addEventListener("monetizationstart", () => {
+        console.log("Enabled web monetization")
+        setVisible(true)
+      })
+    }
   }
 
   return (
     <div>
       <Switch checkedChildren="View as non-monetized user" unCheckedChildren="View as monetized user"
-              onChange={() => setAlwaysVisible(!alwaysVisible)} style={{marginBottom: "10px"}}/>
+              onChange={() => setAlwaysVisible(!alwaysVisible)} style={{ marginBottom: "10px" }}/>
       <div>
         {(visible || alwaysVisible) && <div>{children}</div>}
       </div>
